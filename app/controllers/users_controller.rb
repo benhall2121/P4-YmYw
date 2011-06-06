c0eb9557c09c01ee7de64e3f6b96e6a09a690678
Ben Hall <benhall2121@yahoo.com>
2011-06-06 09:51:34 -0700

    benhall2121@yahoo.com a few minor cleanups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -93,7 +93,7 @@ class UsersController < ApplicationController
     
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      redirect_to(root_path) unless current_user
     end
 
 end
